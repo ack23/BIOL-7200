ae8d89a3d5ac52564d7969d60b1f0f0e8a1b9055
ack23 <ashleyckim1@gmail.com>
2025-09-09 21:52:57 -0400

    adding tblastn

--- find_homologs.sh
+++ find_homologs.sh
@@ -8,3 +8,5 @@ tblastn -query $query -subject $subject -outfmt '6 std qlen' \
 query="$1"
 subject="$2"
 out="$3"
+tblastn -query "$query" -subject "$subject" -outfmt '6 std qlen' \
+| awk '$3 > 30 && $4 >= 0.9 * $13' > "$out"
