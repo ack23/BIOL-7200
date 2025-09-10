ec2258c1253976293d8dfdcd4a80a5192a36a9f6
ack23 <ashleyckim1@gmail.com>
2025-09-09 21:59:32 -0400

    remove duplicates

--- find_homologs.sh
+++ find_homologs.sh
@@ -1,13 +1,9 @@
 #!/usr/bin/env bash
-query=$1
-subject=$2
-out=$3
-tblastn -query $query -subject $subject -outfmt '6 std qlen' \
-| awk '$3 > 30 && $4 >= 0.9 * $13' > $out
-| wc -l $out
 query="$1"
 subject="$2"
 out="$3"
+
 tblastn -query "$query" -subject "$subject" -outfmt '6 std qlen' \
 | awk '$3 > 30 && $4 >= 0.9 * $13' > "$out"
+
 count=$(wc -l < "$out")
